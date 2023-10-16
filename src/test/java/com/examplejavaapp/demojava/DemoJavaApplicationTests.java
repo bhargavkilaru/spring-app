afcc3c448ecf2f6a558e0794a8d8a2bfc4c7b910
bhargavk27 <bhargavkilaru27@gmail.com>
2023-10-16 11:31:33 -0400

    First commit

---
+++ src/test/java/com/examplejavaapp/demojava/DemoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplejavaapp.demojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
